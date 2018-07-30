da5df7dc24e7d19fbc423cfa76fbc66924377d2a
Peter Banka <pbanka@fastly.com>
2018-07-30 13:18:58 -0700

    Simple working system

--- src/App.js
+++ src/App.js
@@ -1,18 +1,41 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import HeaderBar from './HeaderBar';
 import './App.css';
+
+const INVENTORY = [
+  {category: "Sporting Goods", price: "$49.99", stocked: true, name: "Football"},
+  {category: "Sporting Goods", price: "$9.99", stocked: true, name: "Baseball"},
+  {category: "Sporting Goods", price: "$29.99", stocked: false, name: "Basketball"},
+  {category: "Electronics", price: "$99.99", stocked: true, name: "iPod Touch"},
+  {category: "Electronics", price: "$399.99", stocked: false, name: "iPhone 5"},
+  {category: "Electronics", price: "$199.99", stocked: true, name: "Nexus 7"}
+];
 
 class App extends Component {
   render() {
+    let tableGuts = INVENTORY.map(currentValue => {
+      let style
+      if (currentValue.stocked === false) {
+        style = {color: 'red'}
+      }
+      return <tr><td style={style}>{currentValue.name}</td><td>{currentValue.price}</td></tr>
+    })
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <HeaderBar />
+        <input placeholder="Search..."/>
+        <br/>
+        <label>
+          <input type="checkbox" />
+          Only show products in stock
+        </label>
+        <br/>
+        <table>
+          <tbody>
+            {tableGuts}
+          </tbody>
+        </table>
       </div>
     );
   }
